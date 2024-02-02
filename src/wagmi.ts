175d1f4244ed2f596ffc0597c4ed06bce14268b2
Paul <web3samuraidev@gmail.com>
2024-02-02 02:01:48 -0400

    chore: update for oort

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,38 +1,26 @@
 import { getDefaultWallets, connectorsForWallets } from "@rainbow-me/rainbowkit";
-import { argentWallet, ledgerWallet } from "@rainbow-me/rainbowkit/wallets";
 import { configureChains, createConfig } from "wagmi";
 import {
-  mainnet,
-  sepolia,
-  polygon,
-  polygonMumbai,
-  optimism,
-  optimismGoerli,
-  arbitrum,
-  arbitrumGoerli,
-  zkSync,
-  zkSyncTestnet,
-  base,
-  baseGoerli,
+  oortMainnetDev,
 } from "wagmi/chains";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 
-const alchemyApiKey = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY;
 const walletConnectProjectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID;
 
-if (!alchemyApiKey || !walletConnectProjectId) {
+if (!walletConnectProjectId) {
   throw new Error("Some ENV variables are not defined");
 }
 
-const { chains, publicClient, webSocketPublicClient } = configureChains(
+const { chains, publicClient } = configureChains(
+  [oortMainnetDev],
   [
-    ...(process.env.NODE_ENV === "production"
-      ? [mainnet, optimism, polygon, arbitrum, zkSync, base]
-      : [sepolia, optimismGoerli, polygonMumbai, arbitrumGoerli, zkSyncTestnet, baseGoerli]),
+    jsonRpcProvider({
+      rpc: (chain) => ({
+        http: `https://dev-rpc.oortech.com`,
+      }),
+    }),
   ],
-  [alchemyProvider({ apiKey: alchemyApiKey }), publicProvider()],
-);
+)
 
 const { wallets } = getDefaultWallets({
   appName: "Next-Web3-Boilerplate",
@@ -45,8 +33,6 @@ const connectors = connectorsForWallets([
   {
     groupName: "Other",
     wallets: [
-      argentWallet({ projectId: walletConnectProjectId, chains }),
-      ledgerWallet({ projectId: walletConnectProjectId, chains }),
     ],
   },
 ]);
@@ -55,7 +41,6 @@ export const config = createConfig({
   autoConnect: true,
   connectors,
   publicClient,
-  webSocketPublicClient,
 });
 
 export { chains };
